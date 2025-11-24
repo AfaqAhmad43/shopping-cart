c78f2bd0425bff2cd116769b4bdf50db881f161c
SuperPsycho4347 <superpsycho4347@gmail.com>
2025-11-24 19:45:36 +0500

    -added initial skeleton for the Home page.

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+
+export default function Home() {
+    return(
+        <div className="home-page">
+            <Navbar />
+            <h1>Welcome to the shopping cart home page!</h1>
+            <h2>We hope you enjoy your shopping.</h2>
+        </div>        
+    )
+}
