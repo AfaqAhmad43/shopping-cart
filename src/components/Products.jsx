dead8101a0a0dbd4575da7e7fb2491e90c095dc2
SuperPsycho4347 <superpsycho4347@gmail.com>
2025-11-24 20:18:24 +0500

    -added initial display of response divs.

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 export default function Products() {
     const [prod, setProd] = useState(false)
+    const [data, setData] = useState([])
 
     useEffect(() => {
         const fetchProducts = async () => {
@@ -11,8 +12,10 @@ export default function Products() {
                     throw new Error(`HTTP error! status: ${response.status}`)
                 }
                 const jsonData = await response.json();
-                console.log(jsonData)
-                setProd(jsonData)
+                const trimJson = jsonData.splice(0, 9)
+                console.log(trimJson)
+                setProd(true)
+                setData(jsonData)
             }
             catch(error) {
                 console.error("Error fetching data:", error)
@@ -20,4 +23,26 @@ export default function Products() {
         }
         fetchProducts();
     }, [])
+
+    return (
+        <div className="prod-group">
+            {prod ? (
+                <ul className="product">
+                    {data.map(item => (
+                        <div className="prod-info">
+                        <div className="prod-img">
+                            <img src={item.image}/>
+                        </div>
+                        <div className="prod-text">
+                            <li key={item.id}>{item.title}</li>
+                       </div>
+                        </div>
+                    ))}
+                </ul>
+            ) : (
+                <p>Loading data...</p>
+            )
+            }
+        </div>
+    )
 }
