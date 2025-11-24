cf148e4f8e49ccf25e03955d1c3a63089812f491
SuperPsycho4347 <superpsycho4347@gmail.com>
2025-11-24 19:42:42 +0500

    -added navbar component.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+    return (
+        <div className="nav-bar">
+            <h1>Shopping Cart</h1>
+            <div className="nav-group">
+                <ul>
+                    <li>Home</li>
+                    <li>Products</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
